46ab88d3734ac1d49686d2397f35327aaf4bf67b
DESKTOP-S7FFHIC\h <m.alamrousi.work2@gmail.com>
2024-10-27 20:24:11 +0300

    Integrate RegisterCubit for Register Screen and Refactor Sign-Up Form

--- lib/core/routing/app_routes.dart
+++ lib/core/routing/app_routes.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:uptodo/core/di/dependency_injection.dart';
+import 'package:uptodo/feature/register/ui/cubit/register_cubit/register_cubit.dart';
 import 'package:uptodo/feature/register/ui/view/register_screen.dart';
 
 import '../../feature/login/ui/view/login_screen.dart';
 import '../../feature/on_boading/ui/on_boarding_screen.dart';
 import '../../feature/start_screen/ui/start_screen.dart';
 import 'routes.dart';
+
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class AppRoutes {
   static Route generateRoute(RouteSettings settings) {
@@ -13,10 +17,14 @@ class AppRoutes {
         return MaterialPageRoute(builder: (_) => const OnBoardingScreen());
       case Routes.startScreen:
         return MaterialPageRoute(builder: (_) => const StartScreen());
-        case Routes.loginScreen:
-        return   MaterialPageRoute(builder: (_) => const LoginScreen());
-        case Routes.registerScreen:
-        return   MaterialPageRoute(builder: (_) => const RegisterScreen());
+      case Routes.loginScreen:
+        return MaterialPageRoute(builder: (_) => const LoginScreen());
+      case Routes.registerScreen:
+        return MaterialPageRoute(
+            builder: (_) => BlocProvider(
+                  create: (context) => getIt.get<RegisterCubit>(),
+                  child: const RegisterScreen(),
+                ));
 
       default:
         return MaterialPageRoute(
--- lib/feature/register/ui/view/register_screen.dart
+++ lib/feature/register/ui/view/register_screen.dart
@@ -4,13 +4,12 @@ import 'package:uptodo/core/widget/build_app_bar.dart';
 import 'package:uptodo/feature/register/ui/view/widgets/custom_divider.dart';
 import 'package:uptodo/feature/register/ui/view/widgets/custom_register_social_auth_button.dart';
 
-import '../../../../../core/helper/app_regx.dart';
 import '../../../../../core/helper/spacing.dart';
 import '../../../../../core/theming/app_styles.dart';
 import '../../../../../core/theming/colors_manger.dart';
 import '../../../../../core/widget/app_text_button.dart';
-import '../../../../../core/widget/app_text_form_filed.dart';
 import '../widgets/aleardy_have_an_account.dart';
+import 'widgets/register_form.dart';
 
 class RegisterScreen extends StatefulWidget {
   const RegisterScreen({super.key});
@@ -38,57 +37,7 @@ class _RegisterScreenState extends State<RegisterScreen> {
                 style: AppStyles.font32WhiteBold,
               ),
               verticalSpacing(53),
-              InputTextFormFiled(
-                keyboardType: TextInputType.emailAddress,
-                inputTextType: 'Username',
-                validator: (value) {
-                  if (value == null ||
-                      value.isEmpty ||
-                      !AppRegex.isNameValid(value)) {
-                    return 'Please enter valid Name';
-                  }
-                },
-                hintText: 'Enter your Email',
-              ),
-                   verticalSpacing(15),
-              InputTextFormFiled(
-                keyboardType: TextInputType.emailAddress,
-                inputTextType: 'Email',
-                validator: (value) {
-                  if (value == null ||
-                      value.isEmpty ||
-                      !AppRegex.isEmailValid(value)) {
-                    return 'Please enter valid Email';
-                  }
-                },
-                hintText: 'Enter your Email',
-              ),
-              verticalSpacing(15),
-              InputTextFormFiled(
-                keyboardType: TextInputType.visiblePassword,
-                obscureText: obscureTextIcon,
-                inputTextType: 'Password',
-                validator: (value) {
-                  if (value == null ||
-                      value.isEmpty ||
-                      !AppRegex.isPasswordValid(value)) {
-                    return 'Please enter valid Password';
-                  }
-                },
-                hintText: 'Enter your Email',
-                suffixIcon: GestureDetector(
-                  onTap: () {
-                    setState(() {
-                      obscureTextIcon = !obscureTextIcon;
-                    });
-                  },
-                  child: Icon(
-                    obscureTextIcon ? Icons.visibility : Icons.visibility_off,
-                    color: ColorsManger.purpleColor,
-                    size: 22.sp,
-                  ),
-                ),
-              ),
+              const RegisterForm(),
               verticalSpacing(69),
               AppTextButton(
                 backgroundColor: ColorsManger.purpleColor,
@@ -96,7 +45,7 @@ class _RegisterScreenState extends State<RegisterScreen> {
                 textStyle: AppStyles.font16WhiteRegular,
               ),
               verticalSpacing(45),
-             const CustomDivider(),
+              const CustomDivider(),
               verticalSpacing(29),
               const CustomRegisterSocialAuthButton(
                   buttonText: ' Register with Google',
@@ -104,14 +53,15 @@ class _RegisterScreenState extends State<RegisterScreen> {
               const CustomRegisterSocialAuthButton(
                   buttonText: 'Register with Facebook',
                   socialImage: 'assets/svgs/facebook.svg'),
-                         verticalSpacing(46),
-                            const Align(
-                    alignment: Alignment.center,
-                    child:  AlreadyHaveAnAccount()),
+              verticalSpacing(46),
+              const Align(
+                  alignment: Alignment.center, child: AlreadyHaveAnAccount()),
             ],
           ),
         )),
       ),
     );
   }
+
+  
 }
