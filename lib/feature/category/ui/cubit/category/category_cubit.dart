2447cca59acb6c63e735ac19d1a531baba15f536
DESKTOP-S7FFHIC\h <m.alamrousi.work2@gmail.com>
2024-11-02 20:51:43 +0200

    create category state and cubit add categorycubit and categoryrepoimpl in di

---
+++ lib/feature/category/ui/cubit/category/category_cubit.dart
@@ -0,0 +1,37 @@
+import 'package:bloc/bloc.dart';
+import 'package:flutter/material.dart';
+import 'package:meta/meta.dart';
+
+import '../../../data/model/category_model.dart';
+import '../../../data/repo/category_repo_impl.dart';
+
+part 'category_state.dart';
+
+class CategoryCubit extends Cubit<CategoryState> {
+  CategoryCubit(this.categoryRepoImpl) : super(CategoryInitial());
+  final CategoryRepoImpl categoryRepoImpl;
+
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+  final TextEditingController categoryController = TextEditingController();
+
+  Future<void> addCategory(CategoryModel categoryModel) async {
+    emit(CategoryLoading());
+
+    final addCategory = await categoryRepoImpl.addCategory(categoryModel);
+    addCategory.fold((failure) {
+      emit(CategoryFailure(errorMessage: failure.errorMessage));
+    }, (category) {
+      emit(CategorySuccess(categoryModel: category));
+    });
+  }
+
+  Future<void> getCategories(String categoryId) async {
+    emit(CategoryLoading());
+    final getCategory = await categoryRepoImpl.getCategories(categoryId);
+    getCategory.fold((failure) {
+      emit(CategoryFailure(errorMessage: failure.errorMessage));
+    }, (category) {
+      emit(CategorySuccess(categoryModel: category));
+    });
+  }
+}
---
+++ lib/feature/category/ui/cubit/category/category_state.dart
@@ -0,0 +1,15 @@
+part of 'category_cubit.dart';
+
+@immutable
+sealed class CategoryState {}
+
+final class CategoryInitial extends CategoryState {}
+final class CategoryLoading extends CategoryState {}
+final class CategorySuccess extends CategoryState {
+  final CategoryModel categoryModel;
+  CategorySuccess({required this.categoryModel});
+}
+final class CategoryFailure extends CategoryState {
+  final String errorMessage;
+  CategoryFailure({required this.errorMessage});
+}
