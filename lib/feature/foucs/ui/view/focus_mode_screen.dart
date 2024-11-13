b5bae5c1cd619ff0c8285be77a0315ec15acfde1
DESKTOP-S7FFHIC\h <m.alamrousi.work2@gmail.com>
2024-11-13 02:49:34 +0200

    -Create Fouc mode screen - import circular_countdown_timer package - add some info about package in Readme.md

---
+++ lib/feature/foucs/ui/view/focus_mode_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+import 'widgets/focus_mode_screen_body.dart';
+
+class FocusModeScreen extends StatelessWidget {
+  const FocusModeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const FocusModeScreenBody();
+  }
+}
